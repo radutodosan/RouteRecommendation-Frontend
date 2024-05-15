9d4e54579fdb9adedb8b2648699da3ab736ca081
radutosan <radutodosan@yahoo.com>
2024-05-16 01:41:45 +0300

    changed title and description

--- src/app/home/description/description.component.ts
+++ src/app/home/description/description.component.ts
@@ -6,10 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./description.component.css']
 })
 export class DescriptionComponent {
-  title = "Smart City"
-  description="Lucrarea mea de licență are ca scop dezvoltarea și implementarea unui sistem inteligent de recomandare" +
-    " a rutelor adaptat vehiculelor ecologice, concentrându-se pe promovarea mobilității urbane durabile. " +
-    "Sistemul utilizează algoritmi de machine learning pentru  analiza datelor în timp real și pentru a furniza recomandări" +
-    " de rute personalizate și eficiente din punct de vedere al emisiilor pentru șoferii de vehicule ecologice."
+  title = "Sistem inteligent de recomandare a rutelor adaptat vehiculelor ecologice";
+  description="Această lucrare de licență dezvoltă o aplicație web inovatoare " +
+    "care promovează mobilitatea urbană durabilă prin recomandarea rutelor ecologice, folosind algoritmi de învățare automată " +
+    "pentru a oferi rute personalizate și eficiente din punct de vedere al emisiilor. " +
+    "Utilizatorii interacționează printr-o interfață web intuitivă și pot participa la funcționalități sociale " +
+    "pentru a încuraja comportamente de conducere sustenabile. " +
+    "Aplicația creează o comunitate dedicată mobilității verzi și reduce impactul negativ al transportului urban asupra mediului."
 
 }

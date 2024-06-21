fc7d23d68c9be27af7ba83c9519f34401adefd65
radutosan <radutodosan@yahoo.com>
2024-06-22 02:39:58 +0300

    added a new icon and description to home page

--- src/app/home/description/description.component.ts
+++ src/app/home/description/description.component.ts
@@ -7,11 +7,10 @@ import { Component } from '@angular/core';
 })
 export class DescriptionComponent {
   title = "Sistem inteligent de recomandare a rutelor adaptat vehiculelor ecologice";
-  description="Această lucrare de licență dezvoltă o aplicație web inovatoare " +
-    "care promovează mobilitatea urbană durabilă prin recomandarea rutelor ecologice, folosind algoritmi de învățare automată " +
-    "pentru a oferi rute personalizate și eficiente din punct de vedere al emisiilor. " +
-    "Utilizatorii interacționează printr-o interfață web intuitivă și pot participa la funcționalități sociale " +
-    "pentru a încuraja comportamente de conducere sustenabile. " +
-    "Aplicația creează o comunitate dedicată mobilității verzi și reduce impactul negativ al transportului urban asupra mediului."
+  description="Această aplicație web este dedicată promovării mobilității urbane sustenabile. " +
+    "Utilizând seturi date istorice și algoritmi de învățare automată, aplicația recomandă rute eficiente și ecologice " +
+    "pentru mașini electrice, transport public, biciclete și mersul pe jos. " +
+    "Descoperă cum poți reduce emisiile de CO2, evita congestia rutieră și contribui la un mediu mai curat, " +
+    "în timp ce te bucuri de o experiență interactivă și socială alături de prieteni și comunitate."
 
 }
